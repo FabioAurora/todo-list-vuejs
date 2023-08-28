fd3192db8040ca5331520e72afd51b473a021db6
Fabio Aurora <fabioauroradev@gmail.com>
2023-08-28 19:21:26 +0100

    fix: debugged datepicker not showing initial date on mobile

--- src/components/ToDoForm.vue
+++ src/components/ToDoForm.vue
@@ -19,7 +19,7 @@
         name="date"
         type="date"
         onfocus="this.showPicker()"
-        class="cursor-pointer appearance-none bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-52 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        class="cursor-pointer bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-52 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       />
 
       <BaseButton type="submit">Add</BaseButton>
