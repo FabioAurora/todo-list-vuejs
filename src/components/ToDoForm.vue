529a4c43b14ac2f33547d9be1d245969370cbc7e
Fabio Aurora <fabioauroradev@gmail.com>
2023-08-28 21:15:56 +0100

    refactoring

--- src/components/ToDoForm.vue
+++ src/components/ToDoForm.vue
@@ -18,7 +18,6 @@
         id="date"
         name="date"
         type="date"
-        value="dueDate"
         onfocus="this.showPicker()"
         class="cursor-pointer bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 w-52 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       />
